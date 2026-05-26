a65ab145f0ee6c89f69418c2edaa649f91d6fbd0
xGrimCheetah <jeffreymichaelgraham@gmail.com>
2026-05-25 21:47:29 -0700

    chore: update v1.63.1 version comment label

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   // ===== Version =====
-  let APP_VERSION = "1.63.1"; // Color themes
+  let APP_VERSION = "1.63.1"; // Theme palette polish
 
   // ===== Storage & State =====
   const STORE_KEY = 'grocery_tally_v2';
